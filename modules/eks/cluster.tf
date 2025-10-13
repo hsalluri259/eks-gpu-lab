4d06b9194515142b54903194c76091cfa44b9dd3
Harish Salluri <harish.salluri@zynga.com>
2025-10-13 13:59:20 -0700

    removed unused vars

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -22,7 +22,6 @@ module "eks" {
   }
   # enable_irsa = true
   access_entries = {
-    # Example for a role
     my_role_access = {
       principal_arn = var.role_arn
       type          = "STANDARD"
@@ -73,7 +72,7 @@ module "eks" {
 
   }
 
-  # to access cluster objects via kubectl from your local machine without using a bastion host.
+  # To access cluster objects via kubectl from your local machine without using a bastion host.
   endpoint_public_access = true
   # Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
