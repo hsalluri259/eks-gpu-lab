ad53232b909b9ad04be01b2e0825b4beca4f00dc
Harish Salluri <harish.salluri@zynga.com>
2025-10-16 10:40:07 -0700

    add Security group to restrict ssh access

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,6 +1,10 @@
 output "vpc_id" {
-  value = module.vpc.default_vpc_id
+  value = module.vpc.vpc_id
 }
 output "private_subnets" {
   value = module.vpc.private_subnets
+}
+
+output "eks_ssh_sg_id" {
+  value = aws_security_group.eks_ssh.id
 }
